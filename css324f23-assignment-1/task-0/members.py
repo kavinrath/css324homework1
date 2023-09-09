363a90608c041329d87f2d5bbabb60d6a21cbc22
Piraboon Piyawarapong <piraboon77@gmail.com>
2023-09-09 19:41:37 +0700

    yes

--- css324f23-assignment-1/task-0/members.py
+++ css324f23-assignment-1/task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Piraboon Piyawarapong", "sid": "6422781466"},
+    {"name": "Kavinrath Jundang", "sid": "6422781516"},
 ]
